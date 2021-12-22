410d9e4591f919383cef1cd1cf8ff765188b0e46
SIONI Fares <fares.sioni@etu.univ-lyon1.fr>
2021-12-22 14:11:05 +0100

    Ajout pages addPost, myPost, modifyPost, post, profile

--- includes/routes.php
+++ includes/routes.php
@@ -1,6 +1,11 @@
 <?php
 
 $routes = array(
-    'signup' => array('controller' => 'controllerSignup', 'view' => 'viewSignup'),
-    'signin' => array('controller' => 'controllerSignin', 'view' => 'viewSignin')
+    'signup' => array('controller' => 'user/controllerSignup', 'view' => 'user/viewSignup'),
+    'signin' => array('controller' => 'user/controllerSignin', 'view' => 'user/viewSignin'),
+    'addPost' => array('controller' => 'post/controllerAddPost', 'view' => 'post/viewAddPost'),
+    'myPost' => array('controller' => 'post/controllerMyPost', 'view' => 'post/viewMyPost'),
+    'modifyPost' => array('controller' => 'post/controllerModifyPost', 'view' => 'post/viewModifyPost'),
+    'post' => array('controller' => 'post/controllerPost', 'view' => 'post/viewPost'),
+    'profile' => array('controller' => 'user/controllerProfile', 'view' => 'user/viewProfile')
 );
